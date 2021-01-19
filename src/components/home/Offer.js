3e1790db7504d1ad5468e1dd68dc62bac3ef476f
Da-Black-Man <dablackoo@gmail.com>
2021-01-19 12:51:44 -0700

    update

--- src/components/home/Offer.js
+++ src/components/home/Offer.js
@@ -1,12 +1,14 @@
 const Offer = () => {
   return (
-    <section className="c-offer">
-      <div className="o-container">
-        <div className="c-offer__quote">
-          <span className="c-offer__quote-text">
-            "Don't just be good, be Ultimate
-            <em>.</em>"
-          </span>
+    <section data-compontent="offer">
+      <div className="c-offer">
+        <div className="o-container">
+          <div className="c-offer__quote">
+            <span className="c-offer__quote-text">
+              "Don't just be good, be Ultimate
+              <em>.</em>"
+            </span>
+          </div>
         </div>
       </div>
     </section>
