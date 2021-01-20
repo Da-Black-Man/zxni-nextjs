d2f00b1e604a1e0f964e0ad037bf80f74a9f02fc
Da-Black-Man <dablackoo@gmail.com>
2021-01-19 23:02:02 -0700

    update

--- src/components/home/Offer.js
+++ src/components/home/Offer.js
@@ -9,6 +9,19 @@ const Offer = () => {
               <em>.</em>"
             </span>
           </div>
+          <div className="c-offer-main">
+            <div className="c-offer-main__inner">
+              <div className="o-layout -flex -middle">
+                <div className="o-layout__item u-1/1 u-1/2@medium">yo</div>
+                <div className="o-layout__item u-1/1 u-1/2@medium">
+                  <div className="c-offer-main__content">
+                    <h2 className="l-layout__content-title"></h2>
+                    <p className="l-layout__content-text"></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
